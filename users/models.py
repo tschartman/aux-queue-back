c969b6ec4fe7516808fdb8677724e203d05c474d
tim <tschartman@yahoo.com>
2019-12-15 22:30:13 -0600

    create tests for the custom users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
 
 # Create your models here.
+# class Users(AbstractBaseUser):
+#     Username = None
+#     email = models.EmailField(_('email address'), unique=True)
+#     name = models.CharField(max_length=100)
