3c2c9ebf69bc74d905015b3548b146813b02c73f
tim <tschartman@yahoo.com>
2019-12-15 22:50:24 -0600

    changed to subclass off of AbstractBaseUser to control the extra fields

--- users/models.py
+++ users/models.py
@@ -1,13 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
 from .managers import CustomUserManager
 
 
-class CustomUser(AbstractUser):
-    username = None
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
