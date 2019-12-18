7b7e4f0fd4edda58332bab9bad22b84c43998f01
tim <tschartman@yahoo.com>
2019-12-18 01:02:14 -0600

    only logged in users can see their own info and/or change it

--- users/permissions.py
+++ users/permissions.py
@@ -5,3 +5,7 @@ class IsAuthenticatedOrCreate(permissions.IsAuthenticated):
         if request.method == 'POST':
             return True
         return super(IsAuthenticatedOrCreate, self).has_permission(request, view)
+
+class IsUser(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.email == request.user.email
--- users/views.py
+++ users/views.py
@@ -1,12 +1,17 @@
 from users.serializers import UsersSerializer
 from users.models import CustomUser
 from rest_framework import viewsets
-from users.permissions import IsAuthenticatedOrCreate
+from users.permissions import IsAuthenticatedOrCreate, IsUser
 
 class UserViewset(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UsersSerializer
-    permission_classes = (IsAuthenticatedOrCreate,)
+    permission_classes = (IsAuthenticatedOrCreate, IsUser)
+
+    def get_queryset(self):
+        queryset = self.queryset
+        query_set = queryset.filter(email=self.request.user.email)
+        return query_set
 
     def perform_create(self, serializer):
         serializer.save()
