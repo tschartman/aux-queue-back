fb30378798372f58f03c8b347e4a96bb1197b082
tim <tschartman@yahoo.com>
2019-12-23 22:56:28 -0600

    added first and last name to the user model

--- users/models.py
+++ users/models.py
@@ -9,6 +9,8 @@ from .managers import CustomUserManager
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
