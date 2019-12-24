43b5be17e1315840a07b98c5b02ebcf21f313a9e
tim <tschartman@yahoo.com>
2019-12-23 18:59:20 -0600

    fixed user registration

--- users/serializers.py
+++ users/serializers.py
@@ -2,11 +2,18 @@ from rest_framework import serializers
 from users.models import CustomUser
 
 class UsersSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = CustomUser
-        fields = ['email', 'is_staff', 'is_active', 'date_joined']
+        fields = ['email', 'is_staff', 'is_active', 'date_joined', 'password']
         read_only_fields = ['date_joined']
+
+    def create(self, validate_data):
+        user = super(UserSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 class SpotifyAuthSerializer(serializers.ModelSerializer):
 
