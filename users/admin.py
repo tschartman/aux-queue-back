41fc856dced367e4b8072f30884274588e685702
tim <tschartman@yahoo.com>
2019-12-24 01:32:30 -0600

    Finally found the spot to show forms on admin page

--- users/admin.py
+++ users/admin.py
@@ -19,7 +19,7 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('first_name', 'last_name', 'email', 'password', 'is_staff', 'is_active')}
         ),
     )
     search_fields = ('email',)
