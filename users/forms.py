c46498694beea4832ead4f9e5192a371963da72c
tim <tschartman@yahoo.com>
2019-12-24 00:01:54 -0600

    added a form for admin to edit users

--- users/forms.py
+++ users/forms.py
@@ -6,10 +6,10 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = ('email',)
+        fields = ('first_name', 'last_name', 'email',)
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
-        fields = ('email',)
+        fields = ('first_name', 'last_name', 'email',)
