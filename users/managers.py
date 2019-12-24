7b7b5782800ec88dd955449866613b789fa0bf91
tim <tschartman@yahoo.com>
2019-12-24 00:23:46 -0600

    trying to fix admin user manager

--- users/managers.py
+++ users/managers.py
@@ -24,4 +24,4 @@ class CustomUserManager(BaseUserManager):
             raise ValueError(_('Superuser must have is_staff=True.'))
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(email, first_name, last_name, password, **extra_fields)
+        return self.create_user(email, first_name, last_name, password)
