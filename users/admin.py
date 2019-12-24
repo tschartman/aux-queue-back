df8b949901b9cb4183031bb00272e89ab580364b
tim <tschartman@yahoo.com>
2019-12-24 01:35:11 -0600

    one more place to add it

--- users/admin.py
+++ users/admin.py
@@ -13,7 +13,7 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'is_staff', 'is_active',)
     list_filter = ('email', 'is_staff', 'is_active',)
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('first_name', 'last_name', 'email', 'password')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
